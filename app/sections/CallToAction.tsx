bccaf878d8229a78c2729bcef5f692d4ee178041
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-10-04 14:46:08 +1000

    call to action section complete

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -1,5 +1,39 @@
-export const CallToAction = () => {
-  return null;
-};
+import ArrowRight from "@/arrow-right.svg";
+import starImage from "@/star.png";
+import springImage from "@/spring.png";
+import Image from "next/image";
 
-// 2:04:17
+export const CallToAction = () => {
+  return (
+    <section className="overflow-x-clip bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+      <div className="container mx-auto px-8">
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts
+          </p>
+          <Image
+            src={starImage}
+            alt="Star"
+            width={360}
+            className="absolute -top-[137px] -left-[350px]"
+          />
+          <Image
+            src={springImage}
+            alt="Spring"
+            width={360}
+            className="absolute -top-[19px] -right-[331px]"
+          />
+        </div>
+        <div className="mt-10 flex justify-center gap-2">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5 gap-1" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
